19f55f458644c15a227788f1b7b2aa8d1b12d8b6
Andrew Chang <acchang4@gmail.com>
2022-06-07 20:35:23 -0400

    up to 3,2

--- index.js
+++ index.js
@@ -2,10 +2,44 @@ const http = require('http')
 const express = require('express')
 const app = express()
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/plain' })
-  response.end('Hello World')
+let persons = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello World!</h1>')
+  })
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+  })
+
+app.get('/info', (request, response) => {
+    const currentDate = new Date()
+    response.send(`<h2>Phonebook has info for ${persons.length} people</h2>
+    <h2>${currentDate}</h2>`)
 })
+
 
 const PORT = 3001
 app.listen(PORT)
